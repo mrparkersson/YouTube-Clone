f786dfaa164e0b39241c3defa78c8d1b7adf29d8
mrparkersson <amoparkerprince78@gmail.com>
2022-09-18 11:46:32 +0000

    Make final changes to make page responsive

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 import {
-  demoThumbnailUrl,
   demoVideoUrl,
   demoVideoTitle,
   demoChannelUrl,
@@ -19,7 +18,7 @@ const VideoCard = ({
   return (
     <Card
       sx={{
-        width: { md: '320px', xs: '100%' },
+        width: { xs: '100%', sm: '358px', md: '320px' },
         boxShadow: 'none',
         borderRadius: 0,
       }}
@@ -28,7 +27,7 @@ const VideoCard = ({
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: { xs: '100%', sm: '358px' }, height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -2,19 +2,31 @@ import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import { Box, Stack, Typography } from '@mui/material';
-import { CheckBox } from '@mui/icons-material';
-import { Video } from './';
+import { CheckCircle } from '@mui/icons-material';
+import { Videos } from './';
 import { fetchData } from '../utils/getDataFromApi';
 
 const VideoContainer = () => {
   const [video, setVideo] = useState(null);
+  const [populatedVideos, setPopulatedVideos] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
     fetchData(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideo(data?.items[0])
     );
+
+    fetchData(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setPopulatedVideos(data.items)
+    );
   }, [id]);
+
+  if (!video?.snippet) return 'Loading...';
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = video;
 
   return (
     <Box>
@@ -27,9 +39,44 @@ const VideoContainer = () => {
               className="react-player"
             />
             <Typography variant="h5" color="#fff" fontWeight="bold" p={2}>
-              {video?.snippet?.title}
+              {title}
             </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: '#fff' }}
+              py={1}
+              px={2}
+            >
+              <Link to={`/channel/${channelId}`}>
+                <Typography
+                  variant={{ sm: 'subtitle1', md: 'h6' }}
+                  color="#fff"
+                >
+                  {channelTitle}
+                  <CheckCircle
+                    sx={{ fontSize: '12px', color: 'gray', ml: '5px' }}
+                  />
+                </Typography>
+              </Link>
+              <Stack direction="row" gap="20px" alignItemns="center">
+                <Typography variant="body1" sx={{ opacity: '0.7' }}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+                <Typography variant="body1" sx={{ opacity: '0.7' }}>
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
           </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={populatedVideos} direction="column" />
         </Box>
       </Stack>
     </Box>
